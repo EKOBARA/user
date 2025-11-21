9f336ece83f820b49850fdaabfe36ba12aa91cd2
ekobara <emmanuelkobara@gmail.com>
2025-11-20 20:23:03 -0500

    Keycloak manager

--- src/main/java/com/example/user/config/KeyCloakManager.java
+++ src/main/java/com/example/user/config/KeyCloakManager.java
@@ -1,4 +1,16 @@
 package com.example.user.config;
 
+import lombok.RequiredArgsConstructor;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class KeyCloakManager {
+
+    private final KeyCloakProperties keyCloakProperties;
+
+    public RealmResource getKeycloakInstanceWithRealm() {
+        return keyCloakProperties.getKeycloakInstance().realm(keyCloakProperties.getRealm());
+    }
 }
